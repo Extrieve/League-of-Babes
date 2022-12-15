b36ab4a2fb3fe855f1111faf9db6356070f8a304
Extrieve <nicolas.cinera@gmail.com>
2022-12-15 15:28:31 -0500

    Initial Project Setup

---
+++ src/main/java/com/leaguebabe/LeagueOfBabesApplication.java
@@ -0,0 +1,13 @@
+package com.leaguebabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfBabesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueOfBabesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leaguebabe/LeagueOfBabesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaguebabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfBabesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
