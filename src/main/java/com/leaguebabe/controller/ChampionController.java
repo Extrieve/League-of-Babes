d3af10158c059b132d3494be8506bdb84a861561
Extrieve <nicolas.cinera@gmail.com>
2022-12-16 17:41:47 -0500

    Added REST Mappings

--- src/main/java/com/leaguebabe/controller/ChampionController.java
+++ src/main/java/com/leaguebabe/controller/ChampionController.java
@@ -1,11 +1,12 @@
 package com.leaguebabe.controller;
 
+import com.leaguebabe.entity.Champion;
 import com.leaguebabe.service.ChampionDelegator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 public class ChampionController {
@@ -18,8 +19,18 @@ public class ChampionController {
         return ResponseEntity.ok().body("THIS IS HOME");
     }
 
+    @GetMapping("/champions")
+    public ResponseEntity<Collection<Champion>> getAllChampions(){
+        return championDelegator.getAllChampions();
+    }
+
     @GetMapping("champion/{name}")
     public ResponseEntity<String> championUrl(@PathVariable String name){
         return championDelegator.getProfilePictureByChampionName(name);
+    }
+
+    @PostMapping(value = "champion", consumes = "application/json")
+    public ResponseEntity<String> saveChampion(@RequestBody Champion championToSave){
+        return championDelegator.saveChampion(championToSave);
     }
 }
--- src/main/java/com/leaguebabe/service/ChampionDelegator.java
+++ src/main/java/com/leaguebabe/service/ChampionDelegator.java
@@ -2,9 +2,12 @@ package com.leaguebabe.service;
 
 import com.leaguebabe.entity.Champion;
 import com.leaguebabe.repository.ChampionRepo;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class ChampionDelegator implements ServiceDelegator{
@@ -12,10 +15,20 @@ public class ChampionDelegator implements ServiceDelegator{
     @Autowired
     private ChampionRepo championRepo;
 
+    public ResponseEntity<Collection<Champion>> getAllChampions(){
+        return ResponseEntity.ok().body(championRepo.findAll());
+    }
+
     public ResponseEntity<String> getProfilePictureByChampionName(String championName){
 
-        String url = championRepo.findByName(championName).getName();
+        String url = championRepo.findByName(championName).getProfilePictureUrl();
 
         return ResponseEntity.ok().body(url);
+    }
+
+    @Transactional
+    public ResponseEntity<String> saveChampion(Champion champion){
+        championRepo.save(champion);
+        return ResponseEntity.ok().body("Champion saved");
     }
 }
