2b269d3f21bc2caf03d5c484c56f021c7ff05f68
Extrieve <nicolas.cinera@gmail.com>
2022-12-19 11:39:46 -0500

    Logger and small refactoring

--- src/main/java/com/leaguebabe/service/ChampionDelegator.java
+++ src/main/java/com/leaguebabe/service/ChampionDelegator.java
@@ -3,32 +3,51 @@ package com.leaguebabe.service;
 import com.leaguebabe.entity.Champion;
 import com.leaguebabe.repository.ChampionRepo;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
 
 @Service
+@Slf4j
 public class ChampionDelegator implements ServiceDelegator{
 
     @Autowired
     private ChampionRepo championRepo;
 
+    Logger logger = org.slf4j.LoggerFactory.getLogger(ChampionDelegator.class);
+
     public ResponseEntity<Collection<Champion>> getAllChampions(){
-        return ResponseEntity.ok().body(championRepo.findAll());
+        logger.info("Getting all champions");
+        List<Champion> allChamps = championRepo.findAll();
+
+        if(allChamps.isEmpty()){
+            logger.warn("No champions found");
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(allChamps);
     }
 
     public ResponseEntity<String> getProfilePictureByChampionName(String championName){
+        logger.info("Finding champion with name" + championName);
 
-        String url = championRepo.findByName(championName).getProfilePictureUrl();
-
-        return ResponseEntity.ok().body(url);
+        try{
+            Champion champion = championRepo.findByName(championName);
+            return ResponseEntity.ok().body(champion.getProfilePictureUrl());
+        }
+        catch (Exception e){
+            logger.warn("Champion not found");
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @Transactional
     public ResponseEntity<String> saveChampion(Champion champion){
         championRepo.save(champion);
-        return ResponseEntity.ok().body("Champion saved");
+        return ResponseEntity.ok().body("Champion with name" + champion.getName() + " saved");
     }
 }
