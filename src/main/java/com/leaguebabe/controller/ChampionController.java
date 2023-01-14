3910440a3dcb4f827edeec5b1eb773cdf3ea0949
Extrieve <nicolas.cinera@gmail.com>
2023-01-14 08:33:18 -0500

    Enabled Cross Origin for Local Dev

--- src/main/java/com/leaguebabe/controller/ChampionController.java
+++ src/main/java/com/leaguebabe/controller/ChampionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*")
 public class ChampionController {
 
     @Autowired
