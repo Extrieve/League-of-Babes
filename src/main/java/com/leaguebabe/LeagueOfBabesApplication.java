558fff3043d5bbe33c8c0c8475a5fb512cc09451
Nick <nicolas.cinera@gmail.com>
2023-02-01 13:04:05 -0500

    CommandlineRunner for Transactions

--- src/main/java/com/leaguebabe/LeagueOfBabesApplication.java
+++ src/main/java/com/leaguebabe/LeagueOfBabesApplication.java
@@ -1,5 +1,9 @@
 package com.leaguebabe;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leaguebabe.entity.Champion;
+import com.leaguebabe.repository.ChampionRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class LeagueOfBabesApplication {
@@ -16,32 +22,21 @@ public class LeagueOfBabesApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(ChampionRepo championRepo) {
 		return args -> {
-			System.out.println("Running python script");
-			// Current working directory
-			String cwd = System.getProperty("user.dir");
-			System.out.println("Current working directory : " + cwd);
-			String[] command = new String[]{"python", cwd + "/src/main/java/com/body/improvement/club/utility/transactions.py"};
-			Process process = Runtime.getRuntime().exec(command);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-			}
-
-			BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-			String errorLine;
-			while ((errorLine = errorReader.readLine()) != null) {
-				System.out.println("Error: " + errorLine);
-			}
-
-			int exitStatus = process.waitFor();
-			if (exitStatus == 0) {
-				System.out.println("Python script ran successfully");
-			} else {
-				System.out.println("Python script failed");
-			}
+			// Get working directory
+			String workingDir = System.getProperty("user.dir");
+			// Read in JSON file located in the resources folder
+			BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("AllChampions.json"))));
+			// Hold the json file as a List<Champion>
+			 List<Champion> champions = new ObjectMapper().readValue(reader, new TypeReference<List<Champion>>(){});
+			 champions.forEach(
+					 champion -> {
+						 System.out.println(champion.getName());
+					 }
+			 );
+//			 Save the champions to the database
+			championRepo.saveAll(champions);
 		};
 	}
 
