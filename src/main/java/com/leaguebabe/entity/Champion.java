cc58f1b9dc4ac9895a09ccec2f396244089fd1d8
Nick <nicolas.cinera@gmail.com>
2023-05-08 18:17:14 -0400

    Replacing Lombok Implementation for Equals and Hashcode to improve performance

--- src/main/java/com/leaguebabe/entity/Champion.java
+++ src/main/java/com/leaguebabe/entity/Champion.java
@@ -3,6 +3,9 @@ package com.leaguebabe.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
 
 @Entity
 @Builder
@@ -10,7 +13,6 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode
 public class Champion {
 
     @Id
@@ -19,4 +21,17 @@ public class Champion {
     private String blurb;
     private String profilePictureUrl;
     private String[] tags;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Champion champion = (Champion) o;
+        return getName() != null && Objects.equals(getName(), champion.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
